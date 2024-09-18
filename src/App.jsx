1bfd701ed24c53650ed0cc4f38ab8b7b77dbcbd9
nabi1110 <nabinabi1110@gmail.com>
2024-09-18 21:02:32 +0900

    App.jsx 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import profileIcon from "./assets/icons/Header/profileIcon.png";
+import LoginPage from "./pages/LoginPage";
 import LayOut from "./components/commmon/LayOut";
 import MyPage from "./pages/MyPage";
 import MainPage from "./pages/MainPage";
@@ -10,10 +11,18 @@ function App() {
   return (
     <Routes>
       <Route>
+        <Route path="/LoginPage" element={<LoginPage />} />
+
         <Route element={<LayOut />}>
           <Route
             path="/MyPage"
-            element={<MyPage profileIcon={profileIcon} />}
+            element={
+              <MyPage
+                profileIcon={profileIcon}
+                name={"포폴만들조"}
+                nickname={"폴리오프레임"}
+              />
+            }
           />
           <Route path="/MainPage" element={<MainPage />} />
         </Route>
