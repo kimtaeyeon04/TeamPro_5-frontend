31348e376e652219c9bb74921c59a108e045ab88
Kimtaeyeon04 <te4939@naver.com>
2024-09-26 16:38:16 +0900

    메인 페이지 레이아웃 완성

--- src/components/MainPage/Slide.jsx
+++ src/components/MainPage/Slide.jsx
@@ -11,7 +11,10 @@ import 'swiper/css/pagination';
 import N1 from "../../assets/icons/Mainpage/N1.svg";
 import N2 from "../../assets/icons/Mainpage/N2.svg";
 import N3 from "../../assets/icons/Mainpage/N3.svg";
-import job from "../../assets/images/Mainpage/JOB.png";
+import N4 from "../../assets/icons/Mainpage/N4.svg";
+import job from "../../assets/images/Mainpage/JOB.jpg";
+import hackathon from "../../assets/images/Mainpage/hackathon.jpg";
+
 
 export default () => {
     const [swiperIndex, setSwiperIndex] = useState(0); // -> 페이지네이션용
@@ -37,67 +40,59 @@ export default () => {
     //   navigation // < > 표시하고 싶다면 주석 X
       pagination={{ clickable: true }}
     >
-        {/* 템플릿 */}
-      <SwiperSlide> 
-            <N1Image src={N1} alt="Number1"/>
-            <VideoWrapper3>
-                <Sharevideo autoPlay loop muted>
-                        <source src="/videos/Sharevideo2.mp4" type="video/mp4" />
-                        비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
-                </Sharevideo>
+        {/* 슬라이드 1 */}
+        <SwiperSlide> 
+                <N1Image src={N1} alt="Number1"/>
+                <VideoWrapper1>
+                    <Sharevideo autoPlay loop muted>
+                            <source src="/videos/Sharevideo2.mp4" type="video/mp4" />
+                            비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
+                    </Sharevideo>
+                    <TextWrapper>
+                        <PointText2>내가 만든 <br></br>포토폴리오를<br></br>공유해보세요</PointText2>
+                        <PointText3>자신이 만든 포토폴리오를 공유해서 다른 사람들과 소통해보세요!</PointText3>
+                        <PointText3>가장 인기있는 포토폴리오를 만들어 볼까요?</PointText3>
+                    </TextWrapper>
+                </VideoWrapper1>
+        </SwiperSlide> 
+
+        {/* 슬라이드 2 */}
+        <SwiperSlide>
+                <N2Image src={N2} alt="Number2"/>
+                <VideoWrapper2>
+                    <HackathonImage src={hackathon} alt="해커톤"/>
                 <TextWrapper>
-                    <PointText2>내가 만든 <br></br>포토폴리오를<br></br>공유해보세요</PointText2>
-                    <PointText3>자신이 만든 포토폴리오를 공유해서 다른 사람들과 소통해보세요!</PointText3>
-                    <PointText3>가장 인기있는 포토폴리오를 만들어 볼까요?</PointText3>
+                    <HackathonText>함께 <br></br>프로젝트해볼까요?</HackathonText>
+                </TextWrapper>
+            </VideoWrapper2>
+        </SwiperSlide>
+
+
+        {/* 슬라이드 3 */}
+        <SwiperSlide>
+                <N3Image src={N3} alt="Number2"/>
+                <VideoWrapper3>
+                    <JOBImage src={job} alt="채용"/>
+                <TextWrapper>
+                    <JobText>채용자에게 <br></br>내 포트폴리오를 <br></br>보여줄 수 있어요</JobText>
                 </TextWrapper>
             </VideoWrapper3>
-      </SwiperSlide>
-    {/* 채용 */}
-      <SwiperSlide>
-            <N3Image src={N3} alt="Number2"/>
+        </SwiperSlide>
 
-            <VideoWrapper3>
-                <JOBImage src={job} alt="채용"/>
-            <Sharevideo autoPlay loop muted>
-                    <source src="/videos/Sharevideo3.mp4" type="video/mp4" />
-                    비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
-            </Sharevideo>
-            <TextWrapper>
-                {/* <PointText2>내가 만든 <br></br>포토폴리오를<br></br>공유해보세요</PointText2> */}
-                <PointText3>나와 맞는 채용자에게 내 포트폴리오를 보여줄 수 있어요!</PointText3>
-                <PointText3>가장 인기있는 포토폴리오를 만들어 볼까요?</PointText3>
-            </TextWrapper>
-        </VideoWrapper3>
-      </SwiperSlide>
-
-      <SwiperSlide>
-            <N2Image src={N2} alt="Number2"/>
-            <VideoWrapper2>
-                <Drawvideo autoPlay loop muted>
-                        <source src="/videos/Drawvideo.mp4" type="video/mp4" />
-                        비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
-                </Drawvideo>
-                <TextWrapper2>
-                    <PointText1>함께 만드는 <br></br>포토폴리오</PointText1>
-                    <StartButton>지금 시작하기</StartButton>
-                </TextWrapper2>
-            </VideoWrapper2>
-      </SwiperSlide>
-
-      <SwiperSlide>
-            <N3Image src={N3} alt="Number3"/>
-            <VideoWrapper2>
-                <Drawvideo autoPlay loop muted>
-                        <source src="/videos/Drawvideo.mp4" type="video/mp4" />
-                        비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
-                </Drawvideo>
-                <TextWrapper>
-                    <PointText1>함께 만드는 <br></br>포토폴리오</PointText1>
-                    {/* <StartButton>지금 시작하기</StartButton> */}
-                </TextWrapper>
-            </VideoWrapper2>
-      </SwiperSlide>
-
+        {/* 슬라이드 4 */}
+        <SwiperSlide>
+                <N4Image src={N4} alt="Number4"/>
+                <VideoWrapper4>
+                    <Drawvideo autoPlay loop muted>
+                            <source src="/videos/Drawvideo.mp4" type="video/mp4" />
+                            비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
+                    </Drawvideo>
+                    <TextWrapper2>
+                        <PointText1>함께 만드는 <br></br>포토폴리오</PointText1>
+                        <StartButton>지금 시작하기</StartButton>
+                    </TextWrapper2>
+                </VideoWrapper4>
+        </SwiperSlide>
     </StyledSwiper>
   );
 };
@@ -118,19 +113,30 @@ const StyledSwiper = styled(Swiper)`
   padding : 0 40px;
 `;
 
-
+//css VideoWrapper 
+const VideoWrapper1 = styled.div`
+    display: flex;
+    align-items: center; 
+    justify-content: space-between; 
+    width: 100%; 
+`; 
 const VideoWrapper2 = styled.div`
     display: flex;
-    margin-top : 18m;
-    width : 100%;
-    margin-left : 3em;
+    align-items: center; 
+    justify-content: space-between; 
+    width: 100%; 
 `;
-
 const VideoWrapper3 = styled.div`
     display: flex;
     align-items: center; 
     justify-content: space-between; 
     width: 100%; 
+`;
+const VideoWrapper4 = styled.div`
+    display: flex;
+    margin-top : 18m;
+    width : 100%;
+    margin-left : 3em;
 `;
 
 const TextWrapper = styled.div`
@@ -140,7 +146,6 @@ const TextWrapper = styled.div`
     left: 40em; 
     margin-top : -5em;   
 `;
-
 const TextWrapper2 = styled.div`
     // display: flex;
     flex-direction: column;
@@ -149,8 +154,7 @@ const TextWrapper2 = styled.div`
     margin-top : 0em;   
 `;
 
-//css video
-
+// css video
 const Sharevideo = styled.video`
     width : 30em;
     margin-left : 10em; 
@@ -198,13 +202,21 @@ const N3Image = styled.img`
     margin-top: 10em;
     padding-right  : 48em;
 `;
-const JOBImage = styled.img`
+const N4Image = styled.img`
     width : 20em;
     margin-top: 10em;
     padding-right  : 48em;
 `;
-
-
+const JOBImage = styled.img`
+    width : 40em;
+    margin-top: 0em;
+    padding-right  : 50em;
+`;
+const HackathonImage = styled.img`
+    width : 38em;
+    margin-top: 0em;
+    padding-right  : 50em;
+`;
 //css text
 const PointText1 = styled.p`
     font-size : 5em;
@@ -227,4 +239,21 @@ const PointText3 = styled.p`
     font-family: "OTF R";
     color : #000;
     margin-bottom : -1em;
+`;
+
+const JobText = styled.p`
+    font-size : 3.2em;
+    font-weight : 900;
+    font-family: "OTF B";
+    color : #6633cc;
+    margin-bottom : 0.2em;
+`;
+
+const HackathonText = styled.p`
+    font-size : 3.2em;
+    font-weight : 900;
+    font-family: "OTF B";
+    color : #6633cc;
+    margin-bottom : 0.2em;
+    margin-left : -1em;
 `;
