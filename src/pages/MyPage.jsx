aaa5c43ccf72c56d571159a26e14ba273ece7836
nabi1110 <nabinabi1110@gmail.com>
2024-09-28 17:24:17 +0900

    적용 버튼 수정 및 공유 버튼 추가

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -20,7 +20,12 @@ function MyPage({ profilePicture, name, nickname }) {
       <MyTemplateContainer className="MyTempalteContainer">
         <MyTemplateTitle className="MyTemplateTitle">내 템플릿</MyTemplateTitle>
         <MyTemplateMenuContainer>
-          <SelectBox />
+          <SelectBoxWrapper>
+            <SelectBox />
+            <ApplyButton onClick={() => console.log("적용 버튼 클릭")}>
+              적용
+            </ApplyButton>
+          </SelectBoxWrapper>
           <SearchBarMini />
         </MyTemplateMenuContainer>
         <Line></Line>
@@ -34,6 +39,10 @@ function MyPage({ profilePicture, name, nickname }) {
             />
           ))}
         </TemplateGrid>
+        <Line></Line>
+        <ButtonWrapper>
+          <ShareButton>공유</ShareButton>
+        </ButtonWrapper>
       </MyTemplateContainer>
     </MyPageContainer>
   );
@@ -76,11 +85,33 @@ const MyTemplateMenuContainer = styled.div`
   justify-content: space-between;
 `;
 
+const SelectBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ApplyButton = styled.button`
+  margin-top: 0.625em;
+  padding: 0.5em 1.5em;
+  width: 5vw;
+  background-color: #0a27a6;
+  color: white;
+  border: none;
+  border-radius: 0.5em;
+  font-size: 1vw;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #092091;
+  }
+`;
+
 const TemplateGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 1em;
   margin-top: 2em;
+  margin-bottom: 2em;
   width: 100%;
   max-width: 80em;
 `;
@@ -88,4 +119,27 @@ const TemplateGrid = styled.div`
 const Line = styled.hr`
   margin: 0.625em 0;
   border: 1px solid #d0d1d9;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const ShareButton = styled.button`
+  margin-top: 0.625em;
+  padding: 0.5em 1.5em;
+  width: 5vw;
+
+  background-color: #0a27a6;
+  color: white;
+  border: none;
+  border-radius: 0.5em;
+  font-size: 1vw;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #092091;
+  }
 `;
--- src/pages/TemplatePage.jsx
+++ src/pages/TemplatePage.jsx
@@ -47,11 +47,12 @@ const SelectBoxWrapper = styled.div`
 const ApplyButton = styled.button`
   margin-top: 0.625em;
   padding: 0.5em 1.5em;
+  width: 5vw;
   background-color: #0a27a6;
   color: white;
   border: none;
   border-radius: 0.5em;
-  font-size: 1em;
+  font-size: 1vw;
   cursor: pointer;
 
   &:hover {
