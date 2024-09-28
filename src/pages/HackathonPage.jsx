57263029908a52ba02892f73130597f4288c7b27
Kimtaeyeon04 <te4939@naver.com>
2024-09-28 20:07:06 +0900

    저장

---
+++ src/pages/HackathonPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+import Card from "../components/commmon/TemplateCard.jsx";
+
+const HackathonPage = () => {
+    return(
+        <>
+        <StudyPreviewWrapper>
+        <Card/>
+        </StudyPreviewWrapper>
+
+        </>
+    );
+};
+
+export default HackathonPage;
+
