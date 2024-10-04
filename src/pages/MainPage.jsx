abd13befb16fad3e47569e9e09213d43608589e8
Kimtaeyeon04 <te4939@naver.com>
2024-10-04 14:36:20 +0900

    메인 깨짐 방지

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -14,72 +14,78 @@ import Slide from "../components/MainPage/slide";
 const MainPage = () => {
     
     return (
-        <>
-        {/* 메인 비디오  */}
-        <MainPageWrapper>
-            <VideoWrapper1>
-                <Mainvideo  autoPlay loop muted>
-                    <source src="/videos/Mainvideo.mp4" type="video/mp4" />
-                    비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
-                </Mainvideo>
-                <Maintext>FolioFrame</Maintext>
-            </VideoWrapper1>
+        <MainContainer>
+            {/* 메인 비디오  */}
+            <MainPageWrapper>
+                <VideoWrapper1>
+                    <Mainvideo  autoPlay loop muted>
+                        <source src="/videos/Mainvideo.mp4" type="video/mp4" />
+                        비디오를 재생할 수 없습니다. 브라우저가 이 형식을 지원하지 않습니다.
+                    </Mainvideo>
+                    <Maintext>FolioFrame</Maintext>
+                </VideoWrapper1>
 
-            <SearchContainer>
-                <SearchInput type="text" placeholder="검색어를 입력해주세요" />
-                <SearchIconWrapper>
-                    <SearchIcon
-                    // onClick={handleSearchClick}
-                    src={searchImg}
-                    alt="search"
-                    />
-                </SearchIconWrapper>
-            </SearchContainer>
+                <SearchContainer>
+                    <SearchInput type="text" placeholder="검색어를 입력해주세요" />
+                    <SearchIconWrapper>
+                        <SearchIcon
+                        // onClick={handleSearchClick}
+                        src={searchImg}
+                        alt="search"
+                        />
+                    </SearchIconWrapper>
+                </SearchContainer>
+                
+                {/* 템플릿, 해커톤, 채용 */}
+                <CardWrapper>
+                    <PointText4>
+                        <LogoImage src={Logo} alt="Logo" />
+                        FolioFrame에는 무엇이 있을까요?
+                    </PointText4>
+                    <MainCard />
+                </CardWrapper>
+                
+                {/* 슬라이드 */}
+                <Slide/>
             
-            {/* 템플릿, 해커톤, 채용 */}
-            <CardWrapper>
-                <PointText4>
-                    <LogoImage src={Logo} alt="Logo" />
-                    FolioFrame에는 무엇이 있을까요?
-                </PointText4>
-                <MainCard />
-            </CardWrapper>
+            </MainPageWrapper>
             
-            {/* 슬라이드 */}
-            <Slide/>
-        
-        </MainPageWrapper>
-        
-         {/* 마무리 */}
-        <ThanksWrapper>
-            <PointText4>FolioFrame</PointText4>
-            <PointText5>FolioFrame은 팀프로젝트1의 작업물입니다. 우리의 팀명은 "포폴만들조"이며 김태연, 김예은, 조수연, 최현혜가 함께 했습니다.<br></br> 포트폴리오를 만들때 도움을 줄 수 있는 사이트를 만드는 것이 우리의 목표입니다.<br></br>
-                지금보다 더 나은 서비스를 제공할 수 있도록 노력하겠습니다!!!
-            </PointText5>
-            <CopyWrapper>
-                {/* 구분선 */}
-                <Bar></Bar>
-                <CopyrightImage src={copyright} alt="저작권 표시"/>
-                {/* 메인 영상 저작권 */}
-                <PointText5>평평한 디자인의 모션 그래픽 기하학적 배경_preview</PointText5>
-                <PointText5>https://kr.freepik.com/free-video/motion-graphic-flat-design-geometric-background_3294690#fromView=search&page=1&position=7&uuid=995143c1-4b7b-489b-9250-c1fc132a130b</PointText5>
-                {/* 공유 영상 저작권 */}
-                <PointText5>소셜 미디어 중독자의 모션 그래픽_preview</PointText5>
-                <PointText5>https://kr.freepik.com/free-video/motion-graphic-person-addicted-social-media_3294138#fromView=search&page=6&position=36&uuid=2c0106e1-e052-401a-8156-6a189382987a</PointText5>
-                {/* 리소그래픽 영상 저작권 */}
-                <PointText5>손으로 그린 리소그래프 요소 컬렉션의 모션 그래픽_preview</PointText5>
-                <PointText5>https://kr.freepik.com/free-video/motion-graphic-hand-drawn-risograph-element-collection_3295172#fromView=search&page=1&position=8&uuid=2e65e734-07c3-4cfe-84c9-25c84cf5ea0a</PointText5>
-                {/* 폰트 저작권 */}
-                <PointText5>폰트 https://copyright.keris.or.kr/wft/fntDwnldView?fntGrpId=GFT202408200000000000003</PointText5>
-            </CopyWrapper>
-        </ThanksWrapper>
-        </>
+            {/* 마무리 */}
+            <ThanksWrapper>
+                <PointText4>FolioFrame</PointText4>
+                <PointText5>FolioFrame은 팀프로젝트1의 작업물입니다. 우리의 팀명은 "포폴만들조"이며 김태연, 김예은, 조수연, 최현혜가 함께 했습니다.<br></br> 포트폴리오를 만들때 도움을 줄 수 있는 사이트를 만드는 것이 우리의 목표입니다.<br></br>
+                    지금보다 더 나은 서비스를 제공할 수 있도록 노력하겠습니다!!!
+                </PointText5>
+                <CopyWrapper>
+                    {/* 구분선 */}
+                    <Bar></Bar>
+                    <CopyrightImage src={copyright} alt="저작권 표시"/>
+                    {/* 메인 영상 저작권 */}
+                    <PointText5>평평한 디자인의 모션 그래픽 기하학적 배경_preview</PointText5>
+                    <PointText5>https://kr.freepik.com/free-video/motion-graphic-flat-design-geometric-background_3294690#fromView=search&page=1&position=7&uuid=995143c1-4b7b-489b-9250-c1fc132a130b</PointText5>
+                    {/* 공유 영상 저작권 */}
+                    <PointText5>소셜 미디어 중독자의 모션 그래픽_preview</PointText5>
+                    <PointText5>https://kr.freepik.com/free-video/motion-graphic-person-addicted-social-media_3294138#fromView=search&page=6&position=36&uuid=2c0106e1-e052-401a-8156-6a189382987a</PointText5>
+                    {/* 리소그래픽 영상 저작권 */}
+                    <PointText5>손으로 그린 리소그래프 요소 컬렉션의 모션 그래픽_preview</PointText5>
+                    <PointText5>https://kr.freepik.com/free-video/motion-graphic-hand-drawn-risograph-element-collection_3295172#fromView=search&page=1&position=8&uuid=2e65e734-07c3-4cfe-84c9-25c84cf5ea0a</PointText5>
+                    {/* 폰트 저작권 */}
+                    <PointText5>폰트 https://copyright.keris.or.kr/wft/fntDwnldView?fntGrpId=GFT202408200000000000003</PointText5>
+                </CopyWrapper>
+            </ThanksWrapper>
+        </MainContainer>
+    
     );
 };
 
 export default MainPage;
 
 //css wrapper
+const MainContainer = styled.div`
+    width : 85%;
+    padding: 40px 40px;
+    margin: 0 auto; 
+`;
 const MainPageWrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -109,7 +115,7 @@ const CardWrapper = styled.div`
 `;
 
 const ThanksWrapper = styled.div`
-    margin : 0 6em;
+    // margin : 0 6em;
     margin-top : 6em;
 `;
 
